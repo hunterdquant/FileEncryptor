f475528ac57e5ae1d40e15c31c846584e5fb82ca
Hunter Quant <hunterdquant@gmail.com>
2015-10-29 20:20:49 -0400

    Completed the code for the GuiFileEncryptor.
    Gui is nicely formated.
    Output now uses FileChooser.showSaveDialog, which allows the user to create files for output.

--- src/GuiFileEncrypt.java
+++ src/GuiFileEncrypt.java
@@ -5,9 +5,9 @@
  * A graphical user interface for file encryption.
  */
 
-import javafx.geometry.Insets;
 import java.io.File;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -20,29 +20,37 @@ import javafx.stage.Stage;
 
 public class GuiFileEncrypt extends Application {
 
-	TextField inputKey = new TextField();
-	Text message = new Text();
-	Text outputKey = new Text(); 
-		
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+
+		TextField inputKey = new TextField();
+		Text message = new Text();
+		Text outputKey = new Text(); 
+			
 		primaryStage.setTitle("GUI File Encryptor");
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
-		
+		grid.setPadding(new Insets(25, 25, 25, 25));		
 		
 		Button eButton = new Button("Encrypt");
 		eButton.setOnAction( event -> {
 			File inputFile = getInputFile();
 			File outputFile = getOutputFile();
-			FileEncryptor fe = new FileEncryptor(inputFile, outputFile.getAbsolutePath());
-			fe.textEncrypt();
-			outputKey.setText("" + fe.getEncryptionKey());
-			message.setText("Your message has been encrypted!");
+			if (inputFile != null && outputFile != null) {
+				FileEncryptor fe = new FileEncryptor(inputFile, outputFile.getAbsolutePath());
+				fe.textEncrypt();
+				outputKey.setText("" + fe.getEncryptionKey());
+				message.setText("Your message has been encrypted!");
+			} else {
+				message.setText("You failed to properly select files.");
+			}
 		});
 		
 		Button dButton = new Button("Decrypt");
@@ -52,9 +60,13 @@ public class GuiFileEncrypt extends Application {
 				byte key = Byte.parseByte(inputKey.getText());
 				File inputFile = getInputFile();
 				File outputFile = getOutputFile();
-				FileEncryptor fe = new FileEncryptor(inputFile, outputFile.getAbsolutePath(), key);
-				fe.textDecrypt();
-				message.setText("Your message has been decrypted!");
+				if (inputFile != null && outputFile != null) {
+					FileEncryptor fe = new FileEncryptor(inputFile, outputFile.getAbsolutePath(), key);
+					fe.textDecrypt();
+					message.setText("Your message has been decrypted!");
+				} else {
+					message.setText("You failed to properly select files.");
+				}
 			} catch (NumberFormatException nfe) {
 				message.setText("You entered an invalid key!");
 			}
@@ -62,9 +74,12 @@ public class GuiFileEncrypt extends Application {
 		grid.add(eButton, 0, 0);
 		grid.add(dButton, 0, 1);
 		grid.add(outputKey, 2, 0);
-		grid.add(inputKey, 1, 1);
+		grid.add(inputKey, 2, 1);
 		grid.add(message, 0, 2);
-		grid.add(new Text("Key: "), 1, 0);
+		grid.add(new Text("Key:"), 1, 0);
+		grid.add(new Text("Key:"), 1, 1);
+		GridPane.setColumnSpan(message, 3);
+		
 		Scene scene = new Scene(grid, 400, 200);
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -82,7 +97,7 @@ public class GuiFileEncrypt extends Application {
 		Stage stage = new Stage();
 		FileChooser chooser = new FileChooser();
 		chooser.setTitle("Select a file to save the encrypted/decrypted output.");
-		File inputFile = chooser.showOpenDialog(stage);
+		File inputFile = chooser.showSaveDialog(stage);
 		return inputFile;
 	}
 }
